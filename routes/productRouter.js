aab64966667e1c98bb906d98e17c4270e865ecff
Scott Henning <scott@scotthenning.info>
2022-03-16 14:32:14 -0500

    cross-origin resource sharing

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const cors = require('./cors');
+
 const productRouter = express.Router();
 
 productRouter
   .route('/')
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
   })
-  .get((req, res) => {
+  .get(cors.cors, (req, res) => {
     res.end('Will send all the products to you');
   })
-  .post((req, res) => {
+  .post(cors.corsWithOptions, (req, res) => {
     res.end(
       `Will add the product: ${req.body.name} with description: ${req.body.description} and price: ${req.body.price}`
     );
   })
-  .put((req, res) => {
+  .put(cors.corsWithOptions, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /products');
   })
-  .delete((req, res) => {
+  .delete(cors.corsWithOptions, (req, res) => {
     res.end('Deleting all products');
   });
 module.exports = productRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const cors = require('./cors');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', cors.cors, function (req, res, next) {
   res.send('respond with a resource');
 });
 
